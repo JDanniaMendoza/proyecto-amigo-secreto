50ad1982f91114e91a1386188f2143f2a5f07aa0
Dannia <jakheline97@gmail.com>
2025-03-18 19:05:25 -0500

    Agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    // Capturar el valor del campo de entrada
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    
+    // Validar la entrada
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    
+    // Actualizar el array de amigos
+    amigos.push(nombre);
+    
+    // Limpiar el campo de entrada
+    input.value = "";
+}
